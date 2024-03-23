2271228827cdfbe2416936d11b7e17f97861687c
Alumno <Alumno@LAB1504-12>
2024-03-23 12:56:12 -0500

    Agregar archivos de proyecto.

---
+++ Semana2A/Conductores.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Semana2A
+{
+    /// <summary>
+    /// Lógica de interacción para Conductores.xaml
+    /// </summary>
+    public partial class Conductores : Window
+    {
+        public Conductores()
+        {
+            InitializeComponent();
+        }
+
+        private void Insertar_Click(object sender, RoutedEventArgs e)
+        {
+            Conductores_Ins conductores_Ins = new Conductores_Ins();
+            conductores_Ins.ShowDialog();
+        }
+
+        private void Listar_Click(object sender, RoutedEventArgs e)
+        {
+            Conductores_List conductores_List = new Conductores_List();
+            conductores_List.ShowDialog();
+        }
+    }
+}
---
+++ Semana2A/Operaciones.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Semana2A
+{
+    /// <summary>
+    /// Lógica de interacción para Operaciones.xaml
+    /// </summary>
+    public partial class Operaciones : Window
+    {
+        public Operaciones()
+        {
+            InitializeComponent();
+        }
+
+        private void Ingresos_Click(object sender, RoutedEventArgs e)
+        {
+            Op_Ingresos op_Ingresos = new Op_Ingresos();
+            op_Ingresos.ShowDialog();
+        }
+
+        private void Salidas_Click(object sender, RoutedEventArgs e)
+        {
+            Op_Salidas op_Salidas = new Op_Salidas();
+            op_Salidas.ShowDialog();
+        }
+    }
+}
